9de79e8a0a7ff523a01e2bbbfd06c52393ebfc59
zplzk2 <xupuxps@gmail.com>
2017-10-27 15:41:21 -0700

    Make it work with gRPC 1.4.1.

--- Test.Common/generate_protos.bat
+++ Test.Common/generate_protos.bat
@@ -1,5 +1,5 @@
 @echo off
 
-set TOOLS_PATH=..\packages\Grpc.Tools.1.6.1\tools\windows_x64
+set TOOLS_PATH=..\packages\Grpc.Tools.1.4.1\tools\windows_x64
 
 %TOOLS_PATH%\protoc.exe -I./ --csharp_out ./ Test.proto --grpc_out ./ --plugin=protoc-gen-grpc=%TOOLS_PATH%\grpc_csharp_plugin.exe
